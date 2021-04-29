cb95ef17e1665e29a3e9b3cb4ab02c072c84076c
Sacha Folcke <folcke.sacha@gmail.com>
2021-04-29 21:58:47 +0200

    Mise en place du front - prêt à coder :)

---
+++ front/src/app.js
@@ -0,0 +1,5 @@
+import {render} from "react-dom";
+
+render(
+    <h1>Bienvenue sur Naturama !</h1>,
+    document.querySelector('#content'))
---
+++ front/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'app.bundle.js'
+    },
+    target: ['web', 'es5'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    devtool: 'source-map'
+}
