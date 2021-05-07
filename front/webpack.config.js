a0a06fc47b490da0752e50ad43c1bec849945b91
Gokujuju <wagnerjulien59@gmail.com>
2021-05-07 11:40:02 +0200

    Ajout du design du formulaire login+signup

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'app.bundle.js'
-    },
-    target: ['web', 'es5'],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: 'app.bundle.js',
+	},
+	target: ['web', 'es5'],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(gif|svg|jpg|png)$/,
+				loader: 'file-loader',
+			},
+		],
+	},
+	devtool: 'source-map',
+};
 
-        ]
-    },
-    devtool: 'source-map'
-}
+/*
+Commandes npm supplémentaire
+npm install file-loader --save-dev
+*/
